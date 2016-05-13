de62a2ed0ba81e23da871d0959237ef399885266
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-05-13 16:35:14 -0400

    read script text file, enumerate and collect lists of quotes by character

--- lib/archer_script_scraper.ex
+++ lib/archer_script_scraper.ex
@@ -1,2 +1,13 @@
 defmodule ArcherScriptScraper do
+  require IEx
+  @path "/Users/sophiedebenedetto/Desktop/Dev/archer-bot/archer/data/text/s1e1-Pilot-Mole-Hunt.txt"
+
+  def read_file do 
+    script = File.read(@path)
+    {:ok, text} = script
+    list = String.split(text, "\n")
+    Enum.map(list, fn x -> String.split(x, ":")
+  end
 end
+
+ArcherScriptScraper.read_file
